5e6e8f65b37502454dfeaa251a3466762a5d48d2
Nguyễn Trường <cutruong24701@gmail.com>
2024-03-20 14:58:40 +0700

    fix cors2

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require("express")
-require("dotenv").config()
-const connectDB = require("./src/config/connectDB")
-const indexRoutes = require('./src/routes/indexRoutes')
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./src/config/connectDB");
+const indexRoutes = require("./src/routes/indexRoutes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin: process.env.CLIENT_URL,
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://perfume-tau.vercel.app" || process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-connectDB()
+connectDB();
 
-indexRoutes(app)
+indexRoutes(app);
 
-const port = process.env.PORT || 7979
+const port = process.env.PORT || 7979;
 
 app.listen(port, () => {
-    console.log("Server runing on the port: " + port);
-})
+  console.log("Server runing on the port: " + port);
+});
